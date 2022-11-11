dace9517693a2d0d4d1a890b32459f3299aba422
nik-1x <67558540+nik-1x@users.noreply.github.com>
2022-11-11 05:21:19 +0500

    Release TON Invoices

---
+++ invoices.py
@@ -0,0 +1,28 @@
+import requests
+
+class Invoices:
+
+    payment_domain = "https://pay.astralyx.dev/"
+
+    def __init__(self, token):
+        self.token = token
+
+    def make_request(self, method, parameters):
+        parameters['token'] = self.token
+        res = requests.get(f"https://pay.astralyx.dev/api/{method}", params=parameters)
+        if res.status_code == 200:
+            return res.json()
+        else:
+            return False
+    
+    def createInvoice(self, user_wallet, amount, callback_url):
+        return self.make_request("createInvoice", {
+            "sender": user_wallet, 
+            "amount": amount, 
+            "callback_url": callback_url
+        })
+
+    def getInvoice(self, invoice_id):
+        return self.make_request("getInvoice", {
+            "id": invoice_id
+        })
